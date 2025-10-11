0aba85245ad502ce91c90278ac74038fca415308
Veronika Oschepkova <oschepkova0802@gmail.com>
2025-10-11 21:18:17 +0700

    add getGoods, add search

--- wildberris/js/getGoods.js
+++ wildberris/js/getGoods.js
@@ -1,28 +1,75 @@
 const getGoots = () => {
   const links = document.querySelectorAll(".navigation-link");
 
-  const getData = () => {
+  const renderGoods = (goods) => {
+    const goodsContainer = document.querySelector(".long-goods-list");
+    goodsContainer.innerHTML = "";
+
+    goods.forEach((good) => {
+      const goodBlock = document.createElement("div");
+      goodBlock.classList.add("col-lg-3");
+      goodBlock.classList.add("col-sm-6");
+      goodBlock.innerHTML = `
+      
+      <div class="goods-card">
+              <span class="label ${good.label ? null : "d-none"}">${
+        good.label
+      }</span>
+          <img
+                src="db/${good.img}"
+                alt="${good.name}"
+                class="goods-image"
+              />
+              <h3 class="goods-title">${good.name}</h3>
+        
+              <p class="goods-description">${good.description}</p>
+            
+              <button class="button goods-card-btn add-to-cart" data-id="${
+                good.id
+              }">
+                <span class="button-price">$${good.price} </span>
+              </button>
+            </div>
+      `;
+      goodsContainer.append(goodBlock);
+      console.log(good);
+    });
+  };
+
+  const getData = (value, category) => {
     fetch(
       "https://project111se-default-rtdb.asia-southeast1.firebasedatabase.app/db.json"
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        const array = category
+          ? data.filter((item) => item[category] === value)
+          : data;
+
+        localStorage.setItem("goods", JSON.stringify(array));
+
+        if (window.location.pathname !== "/wildberris/goods.html") {
+          window.location.href = "/wildberris/goods.html";
+        } else {
+          renderGoods(array);
+        }
       });
   };
   links.forEach((link) => {
     link.addEventListener("click", (event) => {
       event.preventDefault();
-      getData();
+      const linkValue = link.textContent;
+      const category = link.dataset.field;
+
+      getData(linkValue, category);
     });
   });
-
-  localStorage.getItem("goods", JSON.stringify(getData));
-  const goods = JSON.parse(localStorage.getItem("goods"));
-  console.log(goods);
-  console.log(localStorage);
-  localStorage.removeItem("goods");
-  console.log(localStorage);
+  if (
+    localStorage.getItem("goods") &&
+    window.location.pathname === "/wildberris/goods.html"
+  ) {
+    renderGoods(JSON.parse(localStorage.getItem("goods")));
+  }
 };
 
 getGoots();
